284d5a772abf6e69438cc4375299061a2cf95859
AMIT MONDOL <amitpstu1@gmail.com>
2021-11-18 11:12:40 +0600

    Initial Commit

---
+++ src/main/java/com/ds/concurrent/fetch/ConcurrentFetchApplication.java
@@ -0,0 +1,13 @@
+package com.ds.concurrent.fetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentFetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/concurrent/fetch/ConcurrentFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.concurrent.fetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
